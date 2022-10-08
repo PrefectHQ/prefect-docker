6cb934086c0f3b85f66465519ec74eff816f9661
Alexander Streed <alex.s@prefect.io>
2022-10-08 10:26:33 -0500

    Updates with latest updates from the template

--- docs/flows.md
+++ docs/flows.md
@@ -1,1 +1,6 @@
+---
+description: 
+notes: This documentation page is generated from source file docstrings.
+---
+
 ::: prefect_docker.flows
--- prefect_docker/flows.py
+++ prefect_docker/flows.py
@@ -1,6 +1,7 @@
 """This is an example flows module"""
 from prefect import flow
 
+from prefect_docker.blocks import DockerBlock
 from prefect_docker.tasks import goodbye_prefect_docker, hello_prefect_docker
 
 
@@ -9,6 +10,14 @@ def hello_and_goodbye():
     """
     Sample flow that says hello and goodbye!
     """
-    print(hello_prefect_docker)
-    print(goodbye_prefect_docker)
+    DockerBlock.seed_value_for_example()
+    block = DockerBlock.load("sample-block")
+
+    print(hello_prefect_docker())
+    print(f"The block's value: {block.value}")
+    print(goodbye_prefect_docker())
     return "Done"
+
+
+if __name__ == "__main__":
+    hello_and_goodbye()
