10a3e860ac7b98092acd553dc9dae1366feccce3
Alexander Streed <desertaxle@users.noreply.github.com>
2023-03-31 08:39:29 -0500

    Adds DockerWorker (#35)
    * Adds DockerWorker
    
    * Adds tests
    
    * Updates test running
    
    * Adds dev requirement
    
    * Adds Buildx
    
    * Changes docker client fixture
    
    * Changes number of processes
    
    * Changes coverage tool
    
    * Adds changelog entry and docs page
    
    * Changes experimental to beta
    
    * Documents DockerWorker attributes
    
    * Removes lazy import
    
    * Adds explicit test timeout

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,83 @@
+import asyncio
+import logging
+import sys
+from contextlib import contextmanager
+from typing import Generator
 from unittest.mock import MagicMock, patch
 
-import docker
+from prefect.docker import IMAGE_LABELS, silence_docker_warnings
+from prefect.testing.fixtures import *  # noqa
+from prefect.testing.utilities import prefect_test_harness
+
+from prefect_docker.worker import CONTAINER_LABELS
+
+with silence_docker_warnings():
+    import docker
+    from docker import DockerClient
+    from docker.models.containers import Container
+    from docker.errors import APIError
+
 import pytest
+
+
+@pytest.fixture(scope="session", autouse=True)
+def prefect_db():
+    """
+    Sets up test harness for temporary DB during test runs.
+    """
+    with prefect_test_harness():
+        yield
+
+
+@pytest.fixture(autouse=True)
+def reset_object_registry():
+    """
+    Ensures each test has a clean object registry.
+    """
+    from prefect.context import PrefectObjectRegistry
+
+    with PrefectObjectRegistry():
+        yield
+
+
+@pytest.fixture(scope="session")
+def event_loop(request):
+    """
+    Redefine the event loop to support session/module-scoped fixtures;
+    see https://github.com/pytest-dev/pytest-asyncio/issues/68
+
+    When running on Windows we need to use a non-default loop for subprocess support.
+    """
+    if sys.platform == "win32" and sys.version_info >= (3, 8):
+        asyncio.set_event_loop_policy(asyncio.WindowsProactorEventLoopPolicy())
+
+    policy = asyncio.get_event_loop_policy()
+
+    if sys.version_info < (3, 8) and sys.platform != "win32":
+        from prefect.utilities.compat import ThreadedChildWatcher
+
+        # Python < 3.8 does not use a `ThreadedChildWatcher` by default which can
+        # lead to errors in tests as the previous default `SafeChildWatcher`  is not
+        # compatible with threaded event loops.
+        policy.set_child_watcher(ThreadedChildWatcher())
+
+    loop = policy.new_event_loop()
+
+    # configure asyncio logging to capture long running tasks
+    asyncio_logger = logging.getLogger("asyncio")
+    asyncio_logger.setLevel("WARNING")
+    asyncio_logger.addHandler(logging.StreamHandler())
+    loop.set_debug(True)
+    loop.slow_callback_duration = 0.25
+
+    try:
+        yield loop
+    finally:
+        loop.close()
+
+    # Workaround for failures in pytest_asyncio 0.17;
+    # see https://github.com/pytest-dev/pytest-asyncio/issues/257
+    policy.set_event_loop(loop)
 
 
 def mock_images_pull(all_tags=False, **kwargs):
@@ -32,7 +108,7 @@ def mock_docker_client():
 @pytest.fixture
 def mock_docker_client_new(mock_docker_client) -> MagicMock:
     with patch.object(
-        docker.DockerClient, "__new__", mock_docker_client
+        DockerClient, "__new__", mock_docker_client
     ) as magic_docker_client:
         yield magic_docker_client
 
@@ -40,7 +116,7 @@ def mock_docker_client_new(mock_docker_client) -> MagicMock:
 @pytest.fixture
 def mock_docker_client_from_env(mock_docker_client) -> MagicMock:
     with patch.object(
-        docker.DockerClient, "from_env", mock_docker_client
+        DockerClient, "from_env", mock_docker_client
     ) as magic_docker_client:
         yield magic_docker_client
 
@@ -61,3 +137,41 @@ def mock_docker_registry_credentials():
     docker_registry_credentials = MagicMock()
     docker_registry_credentials.login.side_effect = mock_login
     return docker_registry_credentials
+
+
+@pytest.fixture(scope="session")
+def docker_client_with_cleanup(worker_id: str) -> Generator[DockerClient, None, None]:
+    client = None
+    try:
+        client = docker.from_env()
+        with cleanup_all_new_docker_objects(client, worker_id):
+            yield client
+    finally:
+        if client is not None:
+            client.close()
+
+
+@contextmanager
+def cleanup_all_new_docker_objects(docker: DockerClient, worker_id: str):
+    IMAGE_LABELS["io.prefect.test-worker"] = worker_id
+    CONTAINER_LABELS["io.prefect.test-worker"] = worker_id
+    try:
+        yield
+    finally:
+        for container in docker.containers.list(all=True):
+            if container.labels.get("io.prefect.test-worker") == worker_id:
+                _safe_remove_container(container)
+            elif container.labels.get("io.prefect.delete-me"):
+                _safe_remove_container(container)
+
+        filters = {"label": f"io.prefect.test-worker={worker_id}"}
+        for image in docker.images.list(filters=filters):
+            for tag in image.tags:
+                docker.images.remove(tag, force=True)
+
+
+def _safe_remove_container(container: Container):
+    try:
+        container.remove(force=True)
+    except APIError:
+        pass
